b1eeee41f1231132edcbd80406bf35b28b046d0c
AYMANEELATRACHE <elatrachayman48@gmail.com>
2026-06-07 19:15:41 +0100

    Database SetUp and Models

---
+++ app/lib/models.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userImage: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const productSchema = new mongoose.Schema(
+  {
+    Title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    Price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    img: {
+      type: String,
+    },
+    size: {
+      type: Number,
+    },
+    Stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    Color: {
+      type: String,
+    },
+    Desc: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
+export const Product = mongoose.model("Product", productSchema);
